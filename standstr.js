37460766c060458aa4a550873246ac50d3a225f4
cookerolo <34161105+cookerolo@users.noreply.github.com>
2018-03-01 18:15:48 -0300

    Update standstr.js

--- standstr.js
+++ standstr.js
@@ -1,4 +1,15 @@
 function standchr(instring) {
     var reg = /^[a-zA-Z\s\-]*$/;
     return instring.match(reg) ? true : false;
-  } 
+  }
+
+function replaceSpace(text, cell) {
+  cellValue = "_" + cell.toUpperCase();
+  reg = /[()]/g;
+  if (!text.match(reg)) {
+    textTrimmed = text;
+  } else {
+    textTrimmed = text.substr(0, text.search(reg)-1);  
+  }                                             
+  return textTrimmed.toUpperCase().replace("OF ","").replace("THE ","").replace(/ /g, "_").concat(cellValue);  
+}
