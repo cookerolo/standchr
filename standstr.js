c831f8908f03df227c5514934ef37e961fbd268c
cookerolo <34161105+cookerolo@users.noreply.github.com>
2018-03-05 18:55:21 -0300

    Update standstr.js

--- standstr.js
+++ standstr.js
@@ -3,13 +3,44 @@ function standchr(instring) {
     return instring.match(reg) ? true : false;
   }
 
+function createTabs() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sh = ss.getActiveSheet();
+  var rData = sh.getDataRange().getValues();
+  var ui = SpreadsheetApp.getUi();
+  var response = ui.alert('This script will create new sheets. Are you sure you want to continue?', ui.ButtonSet.YES_NO);
+  var message = [];      
+  if (response == ui.Button.YES) {
+    for(var i=1, len=rData.length; i<len; i++) {
+      if(rData[i][2] != null || rData[i][2] != "") {         
+       try {      
+          ss.insertSheet(rData[i][2]);
+          ss.getSheetByName(rData[i][2]).getRange('a1').setValue('Date');
+          ss.setActiveSheet(ss.getSheets()[0]);
+          sh.getRange("D"+(i+1)).setValue(new Date());
+       } catch(e) {
+          message.push("row " + (i+1));        
+         }    
+       }
+     }
+     ss.toast("These sheets allready exist: " + message);
+     ss.setActiveSheet(ss.getSheets()[0]);
+   } else {
+      return;
+   }
+}
+
 function replaceSpace(text, cell) {
-  cellValue = "_" + cell.toUpperCase();
+  if (cell=="") {
+    cellValue = "";
+  } else {
+    cellValue = "_" + cell;
+  }  
   reg = /[()]/g;
   if (!text.match(reg)) {
-    textTrimmed = text;
+    textTrimmed = text.trim();
   } else {
-    textTrimmed = text.substr(0, text.search(reg)-1);  
+    textTrimmed = text.trim().substr(0, text.search(reg)-1);  
   }                                             
-  return textTrimmed.toUpperCase().replace("OF ","").replace("THE ","").replace(/ /g, "_").concat(cellValue);  
+  return textTrimmed.toUpperCase().replace("OF ","").replace("THE ","").replace(" &", "").replace(/ /g, "_").concat(cellValue);  
 }
