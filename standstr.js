85387422351d4cf56ffea0ec987f5693ec9f6124
cookerolo <34161105+cookerolo@users.noreply.github.com>
2018-03-07 16:01:30 -0300

    Update standstr.js

--- standstr.js
+++ standstr.js
@@ -3,44 +3,47 @@ function standchr(instring) {
     return instring.match(reg) ? true : false;
   }
 
+function onOpen() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  var menuItems = [
+    {name: 'Create Tabs', functionName: 'createTabs'}
+  ];
+  spreadsheet.addMenu('Scripts', menuItems);
+}
+
 function createTabs() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sh = ss.getActiveSheet();
   var rData = sh.getDataRange().getValues();
   var ui = SpreadsheetApp.getUi();
   var response = ui.alert('This script will create new sheets. Are you sure you want to continue?', ui.ButtonSet.YES_NO);
-  var message = [];      
-  if (response == ui.Button.YES) {
-    for(var i=1, len=rData.length; i<len; i++) {
-      if(rData[i][2] != null || rData[i][2] != "") {         
-       try {      
+  var header = [["Date", "First Name", "Last Name", "Email", "Campaign", "Phone", "Profile Link", "Country", "English Fluency"]];
+  var message = [];    
+
+   if (response == ui.Button.YES) {
+     for(var i=1, len=rData.length; i<len; i++) {
+       if(rData[i][2] != null || rData[i][2] != "") {   
+      
+        try {      
           ss.insertSheet(rData[i][2]);
-          ss.getSheetByName(rData[i][2]).getRange('a1').setValue('Date');
+          ss.getSheetByName(rData[i][2]).getRange('a1:i1').setValues(header);
           ss.setActiveSheet(ss.getSheets()[0]);
           sh.getRange("D"+(i+1)).setValue(new Date());
-       } catch(e) {
+        } catch(e) {
           message.push("row " + (i+1));        
-         }    
+        }    
        }
      }
-     ss.toast("These sheets allready exist: " + message);
+     ss.toast("These sheets already exist: " + message);
      ss.setActiveSheet(ss.getSheets()[0]);
    } else {
       return;
    }
 }
 
-function replaceSpace(text, cell) {
-  if (cell=="") {
-    cellValue = "";
-  } else {
-    cellValue = "_" + cell;
-  }  
+function tabName(text, cell) {
   reg = /[()]/g;
-  if (!text.match(reg)) {
-    textTrimmed = text.trim();
-  } else {
-    textTrimmed = text.trim().substr(0, text.search(reg)-1);  
-  }                                             
+  (cell=="") ? cellValue = "" : cellValue = "_" + cell;  
+  (!text.match(reg)) ? textTrimmed = text.trim() : textTrimmed = text.trim().substr(0, text.search(reg)-1);                                              
   return textTrimmed.toUpperCase().replace("OF ","").replace("THE ","").replace(" &", "").replace(/ /g, "_").concat(cellValue);  
 }
